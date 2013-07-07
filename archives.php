89be309cf26e2ffe9214f1f3c27d275c0885ae96
Valéry Farcy <vfarcy@free.fr>
2013-07-07 14:43:01 +0200

    updates

--- archives.php
+++ archives.php
@@ -1,89 +1,61 @@
-<?php
-/**
- * @package WordPress
- * @subpackage zh2
- */
-/*
-Template Name: Archives
-*/
-?>
-
-<?php get_header(); ?>
-
-		<script type="text/javascript">
-			var root_url = window.location.hostname;
-			function Gsitesearch(curobj){
-				curobj.q.value="site:"+root_url+" "+curobj.qfront.value
-				}
-
-		</script>
-
-
-		<form action="http://www.google.com/search" method="get" onSubmit="Gsitesearch(this)">
-
-			<p><?php echo __('Search'); ?> <?php bloginfo('name'); ?>:<br />
-			<input name="q" type="hidden" class="texta" />
-			<input name="qfront" type="text" style="width: 180px; text-size: 12px; height: 14px;" /> </p>
-
-		</form>
-
-			<?php
-			   if ( has_nav_menu( 'also_see_menu' ) ) {
-			   echo __('<p><strong>Also see:</strong><br />');
-			   wp_nav_menu( array( 'sort_column' => 'menu_order', 'container_class' => 'menu-also-see' ) ); 
-			   echo '</p>'; }
-			?>	
-
-		<table id=arc>
-			<?php
-			$query = "SELECT YEAR(post_date) AS `year`, MONTH(post_date) as `month`, DAYOFMONTH(post_date) as `dayofmonth`, ID, post_name, post_title FROM $wpdb->posts WHERE post_type = 'post' AND post_status = 'publish' ORDER BY post_date DESC";
-			$key = md5($query);
-			$cache = wp_cache_get( 'mp_archives' , 'general');
-			if ( !isset( $cache[ $key ] ) ) {
-			  $arcresults = $wpdb->get_results($query);
-			  $cache[ $key ] = $arcresults;
-			  wp_cache_add( 'mp_archives', $cache, 'general' );
-			} else {
-			  $arcresults = $cache[ $key ];
-			}
-			if ($arcresults) {
-			  $last_year = 0;
-			  $last_month = 0;
-			  foreach ( $arcresults as $arcresult ) {
-			    $year = $arcresult->year;
-			    $month = $arcresult->month;
-			    if ($year != $last_year) {
-			      $last_year = $year;
-			      $last_month = 0;
-			?>
-			<tr class=year><th><br /><br /><?php echo $arcresult->year; ?></th></tr>
-				<?php
-				    }
-				    if ($month != $last_month) {
-				      $last_month = $month;
-				?>
-			<tr class=archive><th><?php echo $wp_locale->get_month($arcresult->month); ?></th><td></td></tr>
-				<?php
-				    }
-				?>
-			<tr class=archive>
-				<th><?php echo $arcresult->dayofmonth; ?></th>
-				<td id=p<?php echo $arcresult->ID; ?>>
-					<a href="<?php echo post_permalink($arcresult->ID); ?>">
-					<?php echo strip_tags(apply_filters('the_title', $arcresult->post_title)); ?></a>
-				</td>
-			</tr>
-			<?php
-			  }
-			}
-			?>
-		</table>
-
-
-
-	</div>  <!-- /end .container -->
-
-	<?php wp_footer(); ?>
-
-</body>
+<?php
+/**
+ * @package WordPress
+ * @subpackage zh2
+ */
+/*
+Template Name: Archives
+*/
+?>
+
+
+
+	<?php get_header(); ?>
+
+<script type="text/javascript">
+			var root_url = window.location.hostname;
+			function Gsitesearch(curobj){
+				curobj.q.value="site:"+root_url+" "+curobj.qfront.value
+				}
+
+		</script>
+
+
+		<form action="http://www.google.com/search" method="get" onSubmit="Gsitesearch(this)">
+
+			<p><h2><?php echo __('Search'); ?></h2> :<br />
+			<input name="q" type="hidden" class="texta" />
+			<input name="qfront" type="text" style="width: 180px; text-size: 12px; height: 14px;" /> </p>
+
+		</form>
+
+<?php
+if(isset($_GET['author_name'])) :
+$curauth = get_userdatabylogin($author_name);
+else :
+$curauth = get_userdata(intval($author));
+endif;
+?>
+
+
+		<h2><?php echo('Archives'); ?> <?php echo $curauth->nickname; ?></h2>
+	
+		<div class="post">
+        <ul class="authorpostslist">
+        
+		<?php if (have_posts()) : ?>
+        <?php query_posts("showposts=-1"); // show one latest post only ?>
+        <?php while (have_posts()) : the_post(); ?>
+        
+        <li><strong><?php the_time('d M Y'); ?>:</strong> <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title(); ?>"><?php the_title(); ?></a></br>
+        </li>
+
+		<?php endwhile; ?>
+		<?php else : ?>
+		<p class="center"><?php _e('No posts by this author.'); ?></p>
+		<?php endif; ?>
+        </ul><!-- /end author posts list -->
+	<?php wp_footer(); ?>
+
+</body>
 </html>
--- author.php
+++ author.php
@@ -1,50 +1,67 @@
-<?php get_header(); ?>
-
-<?php
-if(isset($_GET['author_name'])) :
-$curauth = get_userdatabylogin($author_name);
-else :
-$curauth = get_userdata(intval($author));
-endif;
-?>
-
-
-		<h2><?php echo('Posts by'); ?> <?php echo $curauth->nickname; ?>:</h2>
-	
-		<div class="post">
-        <ul class="authorpostslist">
-        
-		<?php if (have_posts()) : ?>
-        <?php query_posts("showposts=-1"); // show one latest post only ?>
-        <?php while (have_posts()) : the_post(); ?>
-        
-        <li><strong><?php the_time('d M Y'); ?>:</strong> <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title(); ?>"><?php the_title(); ?></a></br>
-        </li>
-
-		<?php endwhile; ?>
-		<?php else : ?>
-		<p class="center"><?php _e('No posts by this author.'); ?></p>
-		<?php endif; ?>
-        </ul><!-- /end author posts list -->
-
-
-
-        <div id="recent-author-comments">
-        <h3><?php _e('Last 10 Comments by '); echo $curauth->display_name; ?></h3>
-        <?php
-$number=10; // number of recent comments to display
-$comments = $wpdb->get_results("SELECT * FROM $wpdb->comments WHERE comment_approved = '1' and comment_author_email='$curauth->user_email' ORDER BY comment_date_gmt DESC LIMIT $number");
-?>
-<ul class="authorpostslist">
-<?php
-if ( $comments ) : foreach ( (array) $comments as $comment) :
-echo '<li class="recentcomments">' . sprintf(__('%1$s on %2$s'), get_comment_date(), '<a href="'. get_comment_link($comment->comment_ID) . '">' . get_the_title($comment->comment_post_ID) . '</a>') . '</li>';
-endforeach; else: ?>
-<p><?php _e('No comments by '); echo $curauth->display_name; ?></p>
-<?php endif; ?>
-</ul>
-</div><!--#recentAuthorComments-->
-
-
-		</div> <!-- /end .post -->
+<?php get_header(); ?>
+
+<script type="text/javascript">
+			var root_url = window.location.hostname;
+			function Gsitesearch(curobj){
+				curobj.q.value="site:"+root_url+" "+curobj.qfront.value
+				}
+
+		</script>
+
+
+		<form action="http://www.google.com/search" method="get" onSubmit="Gsitesearch(this)">
+
+			<p><h2><?php echo __('Search'); ?></h2> :<br />
+			<input name="q" type="hidden" class="texta" />
+			<input name="qfront" type="text" style="width: 180px; text-size: 12px; height: 14px;" /> </p>
+
+		</form>
+
+<?php
+if(isset($_GET['author_name'])) :
+$curauth = get_userdatabylogin($author_name);
+else :
+$curauth = get_userdata(intval($author));
+endif;
+?>
+
+
+		<h2><?php echo('Posts by'); ?> <?php echo $curauth->nickname; ?>:</h2>
+	
+		<div class="post">
+        <ul class="authorpostslist">
+        
+		<?php if (have_posts()) : ?>
+        <?php query_posts("showposts=-1"); // show one latest post only ?>
+        <?php while (have_posts()) : the_post(); ?>
+        
+        <li><strong><?php the_time('d M Y'); ?>:</strong> <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title(); ?>"><?php the_title(); ?></a></br>
+        </li>
+
+		<?php endwhile; ?>
+		<?php else : ?>
+		<p class="center"><?php _e('No posts by this author.'); ?></p>
+		<?php endif; ?>
+        </ul><!-- /end author posts list -->
+
+
+
+        <div id="recent-author-comments">
+        <h3><?php _e('Last 10 Comments by '); echo $curauth->display_name; ?></h3>
+        <?php
+$number=10; // number of recent comments to display
+$comments = $wpdb->get_results("SELECT * FROM $wpdb->comments WHERE comment_approved = '1' and comment_author_email='$curauth->user_email' ORDER BY comment_date_gmt DESC LIMIT $number");
+?>
+<ul class="authorpostslist">
+<?php
+if ( $comments ) : foreach ( (array) $comments as $comment) :
+echo '<li class="recentcomments">' . sprintf(__('%1$s on %2$s'), get_comment_date(), '<a href="'. get_comment_link($comment->comment_ID) . '">' . get_the_title($comment->comment_post_ID) . '</a>') . '</li>';
+endforeach; else: ?>
+<p><?php _e('No comments by '); echo $curauth->display_name; ?></p>
+<?php endif; ?>
+</ul>
+</div><!--#recentAuthorComments-->
+
+
+		</div> <!-- /end .post -->
 <?php get_footer(); ?>
